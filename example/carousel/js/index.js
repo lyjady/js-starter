eab2ddcdee6188fc974036c57e22c5327943a437
991296755@qq.com <lyj112233>
2020-09-09 14:49:23 +0800

    完成轮播图

--- example/carousel/js/index.js
+++ example/carousel/js/index.js
@@ -3,20 +3,30 @@ addEventListener('load', () => {
   const leftArrow = document.querySelector('.arrow-l');
   const rightArrow = document.querySelector('.arrow-r');
   const focus = document.querySelector('.focus');
+  // 节流阀变量
+  let flag = true;
   focus.addEventListener('mouseenter', () => {
     leftArrow.style.display = 'block';
     rightArrow.style.display = 'block';
+    // 如果鼠标进入轮播图则停止播放
+    clearInterval(timer);
+    timer = null;
   })
   focus.addEventListener('mouseleave', () => {
     leftArrow.style.display = 'none';
     rightArrow.style.display = 'none';
+    // 鼠标离开开始播放
+    timer = setInterval(() => {
+      // 调用右侧按钮的点击事件
+      rightArrow.click()
+    }, 2000)
   })
   // 根据图片的数量生成圆点
   const ul = document.querySelector('.focus ul');
   const circle = document.querySelector('.circle');
   const imageWidth = ul.children[0].offsetWidth;
   let currentIndex = 0;
-  for (let i = 0; i < ul.children.length - 1; i++) {
+  for (let i = 0; i < ul.children.length; i++) {
     const li = document.createElement('li');
     // 为小圆点设置索引
     li.setAttribute('data-index', i.toString());
@@ -30,7 +40,7 @@ addEventListener('load', () => {
         // 如果所在图片为最后一个克隆图, 那么就立刻回到第一张图片
         ul.style.left = '0px';
       }
-      animation(ul, -(index * imageWidth + 5), 15)
+      animation(ul, -(index * imageWidth), 15)
       for (let j = 0; j < circle.children.length; j++) {
         circle.children[j].className = '';
       }
@@ -40,6 +50,9 @@ addEventListener('load', () => {
     })
     circle.appendChild(li);
   }
+  // 克隆第一个节点追加到ul中
+  const cloneFirstNode = ul.children[0].cloneNode(true);
+  ul.appendChild(cloneFirstNode);
   // 点击右侧箭头让图片滑动
   rightArrow.addEventListener('click', () => {
     if (currentIndex === ul.children.length - 1) {
@@ -50,7 +63,18 @@ addEventListener('load', () => {
     currentIndex++;
     // 改变小圆点的显示
     changeCircle(circle)
-    animation(ul, -(currentIndex * imageWidth + 5), 15);
+    animation(ul, -(currentIndex * imageWidth), 15);
+  })
+  // 点击左侧箭头让图片滑动
+  leftArrow.addEventListener('click', () => {
+    if (currentIndex === 0) {
+      // 如果是第一张立刻滚动到最后一张克隆图
+      currentIndex = ul.children.length - 1;
+      ul.style.left = -currentIndex * imageWidth + 'px';
+    }
+    currentIndex--;
+    changeCircle(circle);
+    animation(ul, -(currentIndex * imageWidth), 15);
   })
 
   function changeCircle(circle) {
@@ -64,4 +88,10 @@ addEventListener('load', () => {
       circle.children[currentIndex].className = 'current';
     }
   }
+
+  // 设置定时器是轮播图定时播放
+  let timer = setInterval(() => {
+    // 调用右侧按钮的点击事件
+    rightArrow.click()
+  }, 2000)
 })
--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,12 @@
 function animation(element, target, timeout, callback) {
+  console.log(12345);
   clearInterval(element.timer)
   element.timer = setInterval(() => {
     let step = (target - element.offsetLeft) / 10
     step = step > 0 ? Math.ceil(step) : Math.round(step)
     element.style.left = `${element.offsetLeft + step}px`
     if (element.offsetLeft === target) {
-      if (callback) {
-        callback()
-      }
+      callback && callback()
       clearInterval(element.timer)
     }
   }, timeout)
